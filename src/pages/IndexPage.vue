abc797bf12d1c90d39ee9d211ec7a2165de46a2c
gilang <gilang.sukma@digitalsekuriti.id>
2024-03-10 19:50:23 +0700

    init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <!-- <h1>ICIKIWIR</h1> -->
+    <GoogleLogin :callback="callback" />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
